618bee3937cdd74ad6c0616fe3a9bd55791ace86
Tania <thanyasoto30@gmail.com>
2024-05-23 09:59:22 -0400

    add: Navbar y login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,15 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,BsDropdownModule],
+  imports: [CommonModule, RouterOutlet,BsDropdownModule, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ibn-front';
+  title = 'AutoMatrix';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { CrearUsuarioComponent } from './crear-usuario/crear-usuario.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'Home', pathMatch: 'full'},
     {path: 'Home',component: HomeComponent},
+    {path: 'login',component: LoginComponent},
+    {path: 'crearUsuario',component: CrearUsuarioComponent},
     
 ];
